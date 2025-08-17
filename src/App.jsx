f2226bb0c81cdff0e8869530beb17f58e2c6e702
Bhupindar02 <107352499+Bhupindar02@users.noreply.github.com>
2025-08-17 22:15:12 +0530

    Add logic to store token in localStorage after login

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,45 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import {Routes,Route} from 'react-router-dom'
-import Add from "./pages/Add"
-import Orders from "./pages/Orders"
-import List from "./pages/List"
-import { useState } from 'react'
-import Login from './components/Login'
-import { ToastContainer} from 'react-toastify';
-  
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import Orders from "./pages/Orders";
+import List from "./pages/List";
+import { useState } from "react";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
-  const[token,setToken]=useState('')
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):"");
+  //to store the token to local storage (bcz it token get cleared after refreshing)
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  },[token])
   return (
-    <div className='bg-gray-50 min-h-screen'>
-      <ToastContainer/>
-      {token ===""?<Login setToken={setToken}/>:<>
-      <Navbar/>
-      <hr />
-      <div className='flex w-full'>
-        <Sidebar/>
-        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
-         <Routes>
-          <Route path='/add' element={<Add/>}/>
-          <Route path='/list' element={<List/>}/>
-          <Route path='/orders' element={<Orders/>}/>
-         </Routes>
-        </div>
-      </div>
-      </>}
-      
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
